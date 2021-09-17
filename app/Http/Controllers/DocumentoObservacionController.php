8060d3f3cdffc142096959804542eca2cd7c8552
andres085 <andresito.martinez1985@gmail.com>
2021-09-17 15:53:58 -0300

    Migraciones armadas

---
+++ app/Http/Controllers/DocumentoObservacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DocumentoObservacion;
+use Illuminate\Http\Request;
+
+class DocumentoObservacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DocumentoObservacion  $documentoObservacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DocumentoObservacion $documentoObservacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DocumentoObservacion  $documentoObservacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DocumentoObservacion $documentoObservacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DocumentoObservacion  $documentoObservacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DocumentoObservacion $documentoObservacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DocumentoObservacion  $documentoObservacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DocumentoObservacion $documentoObservacion)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_09_17_184437_create_documentos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->foreignId('id_expediente')->constrained('expedientes');
+            $table->foreignId('id_tipo')->constrained('documento_tipos')->unique();
+            $table->foreignId('id_organismo')->constrained('organismos')->unique();
+            $table->year('anio')->unique();
+            $table->string('tema');
+            $table->text('informacion');
+            $table->string('caratula');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documentos');
+    }
+}
