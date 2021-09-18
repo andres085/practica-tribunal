9f623eb0826d176a5bfef402e4fbdfb1e34eda7c
andres085 <andresito.martinez1985@gmail.com>
2021-09-18 10:26:01 -0300

    Factory y seeders de documentos y documentoObservacion listas

---
+++ database/factories/DocumentoFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Documento;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DocumentoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Documento::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+        'id_expediente' => rand(1, 10),
+        'id_tipo' => rand(1, 10),
+        'id_organismo' => rand(1, 10),
+        'id_area' => rand(1, 10),
+        'anio' => $this->faker->year($max = 'now'),
+        'tema' => $this->faker->randomElement(['Rendición', 'Viaticos', 'Documentación', 'Impuestos', 'Vacunación', 'Insumos']),
+        'informacion' => $this->faker->word(),
+        'caratula' => $this->faker->sentence()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\AreaSeeder;
+use Database\Seeders\DocumentoSeeder;
 use Database\Seeders\OrganismoSeeder;
 use Database\Seeders\ExpedienteSeeder;
 use Database\Seeders\DocumentoTipoSeeder;
+use Database\Seeders\DocumentoObservacionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +23,7 @@ class DatabaseSeeder extends Seeder
        $this->call(DocumentoTipoSeeder::class);
        $this->call(OrganismoSeeder::class);
        $this->call(AreaSeeder::class);
+       $this->call(DocumentoSeeder::class);
+       $this->call(DocumentoObservacionSeeder::class);
     }
 }
